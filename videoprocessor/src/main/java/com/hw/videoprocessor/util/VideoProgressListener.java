c5ed77c402ce81b46de4412d698aa52bd879a616
Ben Basha <ben@bigvu.tv>
2019-07-28 14:13:12 +0300

    add on complete and on error callbacks

--- videoprocessor/src/main/java/com/hw/videoprocessor/util/VideoProgressListener.java
+++ videoprocessor/src/main/java/com/hw/videoprocessor/util/VideoProgressListener.java
@@ -1,9 +1,15 @@
 package com.hw.videoprocessor.util;
 
+import androidx.annotation.Nullable;
+
 /**
  * Created by huangwei on 2018/4/17 0017.
  */
 
 public interface VideoProgressListener {
-    void onProgress(float progress);
+    default void onProgress(float progress) {}
+
+    default void onError(@Nullable Exception e) {}
+
+    default void onComplete() {}
 }
